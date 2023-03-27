02510180cbfc958f19260ad97990d60ee043fcd8
OneAfter68 <126597519+OneAfter68@users.noreply.github.com>
2023-03-27 18:40:59 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/58383614-095e-4272-8eec-3277753f8098/guestbook/read-guestbook-entries-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
